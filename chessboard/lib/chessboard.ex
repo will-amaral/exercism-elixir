b57c35881202de5bcaf496bb49d8e66f816dae62
Will Amaral <will@amaral.dev>
2021-10-19 01:28:05 -0300

    Submit chessboard

---
+++ chessboard/lib/chessboard.ex
@@ -0,0 +1,9 @@
+defmodule Chessboard do
+  def rank_range, do: 1..8
+
+  def file_range, do: ?A..?H
+
+  def ranks, do: rank_range() |> Enum.into([])
+
+  def files, do: file_range() |> Enum.into([], &to_string([&1]))
+end
