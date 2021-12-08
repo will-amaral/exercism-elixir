799112d650c66e6e61161d7ffba3d6882cf62f01
Will Amaral <will@amaral.dev>
2021-12-08 11:18:07 -0300

    making changes for transfer

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -1,5 +1,15 @@
 defmodule Bob do
   @spec hey(String.t()) :: String.t()
   def hey(input) do
+    text = String.trim(input)
+    capitals? = String.upcase(text) == text
+    question? = String.last(text) == "?"
+    case {text, capitals?, question?} do
+      {"", _, _} -> "Fine. Be that way!"
+      {_, false, true} -> "Sure."
+      {_, true, false} -> "Whoa, chill out!"
+      {_, true, true} -> "Calm down, I know what I'm doing!"
+      _ -> "Whatever."
+    end
   end
 end
